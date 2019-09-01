8cee9422f33cae8b4901497ce87ddbe7703aeee3
Jason Bothel <shadowmanguyyt@gmail.com>
2019-09-01 09:48:56 -0400

    Modify GSON object instead of recreating it

--- src/main/java/io/minepkg/companion/events/EventServerQueryResponse.java
+++ src/main/java/io/minepkg/companion/events/EventServerQueryResponse.java
@@ -16,7 +16,7 @@ public class EventServerQueryResponse {
     /**
      * Called whenever a minepkg-modded server responds with information about the server and modpack.
      */
-    public static void onServerQueryResponse (CustomServerMetadata metadata) {
+    public static void onCustomServerQueryResponse (CustomServerMetadata customMetadata, ServerMetadata metadata) {
         Modpack modpack = MinepkgCompanion.getModpack();
 
         if (modpack == null) {
@@ -24,8 +24,11 @@ public class EventServerQueryResponse {
             return;
         }
 
-        if (!modpack.getName().equalsIgnoreCase(metadata.minepkgModpack.getName()) ||
-            !modpack.getVersion().equalsIgnoreCase(metadata.minepkgModpack.getVersion())) {
+        String modpackName = customMetadata.minepkgModpack.getName();
+        String modpackVersion = customMetadata.minepkgModpack.getVersion();
+
+        if (!modpack.getName().equalsIgnoreCase(modpackName) ||
+            !modpack.getVersion().equalsIgnoreCase(modpackVersion)) {
             // Modpacks are different
             // Might render an 'X' by the server name here in the future
             return;
