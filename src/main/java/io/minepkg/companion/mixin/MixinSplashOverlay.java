4b5863da70cf5dc5ab091cd945014d5621f18465
Da-Scher <louielouie992@gmail.com>
2022-04-13 15:14:58 -0700

    formatted progress field, kept progress private, renamed methods responsibly

--- src/main/java/io/minepkg/companion/mixin/MixinSplashOverlay.java
+++ src/main/java/io/minepkg/companion/mixin/MixinSplashOverlay.java
@@ -2,6 +2,7 @@ package io.minepkg.companion.mixin;
 
 import io.minepkg.companion.MinepkgCompanion;
 import net.minecraft.client.gui.screen.SplashOverlay;
+import org.apache.logging.log4j.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,15 +12,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(SplashOverlay.class)
 public abstract class MixinSplashOverlay {
     // grab the private progress field.
-    @Shadow float progress;
+    @Shadow private float progress;
 
     // render is the function that changes progress
     @Inject(at = @At("TAIL"), method = "render")
-    private void init(CallbackInfo info) {
+    private void logLoadingProgress(CallbackInfo info) {
 
         // simply log the progress so that the player can see it
         // TODO: Beautify the output
-        MinepkgCompanion.LOGGER.warn(String.format("Loading: %-5f", this.progress));
+        MinepkgCompanion.LOGGER.printf(Level.INFO,"Loading: %6.2f%%", this.progress * 100f);
 
     }
 }
