16c541772fb49d8c67a083ad4621ad5a887e40ab
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2022-08-08 21:55:38 +0200

    Glue (multi-version jar for 1.17-1.19) (#14)
    * split build into '1.17', '1_19' and 'common' subprojects
    
    * move common gradle stuff to root project (and break maven publishing)
    
    * add 'common' dependency
    
    * add Glue:
    - give MixinClientTitleScreen unique names and put both in the mixin config
    - add a mixin plugin choosing between either of them depending on the Minecraft version
    
    * set java version from gradle.properties
    
    * require fabric loader 0.12
    
    * update glue
    
    * add comment for mixin plugin

---
+++ common/src/main/java/io/minepkg/companion/GlueMixinPlugin.java
@@ -0,0 +1,86 @@
+package io.minepkg.companion;
+
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import net.fabricmc.loader.api.Version;
+import net.fabricmc.loader.api.VersionParsingException;
+import net.fabricmc.loader.api.metadata.version.VersionPredicate;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+/** Applies the correct mixin for the given Minecraft version. */
+public class GlueMixinPlugin implements IMixinConfigPlugin {
+	public static final Logger LOGGER = LogManager.getLogger("glue");
+
+	public static boolean test(String modId, String versionRange) {
+		try {
+			Optional<ModContainer> container = FabricLoader.getInstance().getModContainer(modId);
+			if (container.isEmpty())
+				return false;
+
+			VersionPredicate pred = VersionPredicate.parse(versionRange);
+			Version version = container.get().getMetadata().getVersion();
+
+			return pred.test(version);
+		} catch (VersionParsingException e) {
+			LOGGER.error("version matching failed!", e);
+			return false;
+		}
+	}
+
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+		LOGGER.debug("trying to apply {}", mixinClassName);
+
+		if (test("minecraft", ">=1.17 <1.19")) {
+			if (mixinClassName.endsWith("MixinClientTitleScreen1_19")) {
+				LOGGER.debug("skipping");
+				return false;
+			}
+		} else if (test("minecraft", ">=1.19")) {
+			if (mixinClassName.endsWith("MixinClientTitleScreen1_17")) {
+				LOGGER.debug("skipping");
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	@Override
+	public void onLoad(String mixinPackage) {
+
+	}
+
+	@Override
+	public String getRefMapperConfig() {
+		return null;
+	}
+
+	@Override
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+	}
+
+	@Override
+	public List<String> getMixins() {
+		return null;
+	}
+
+	@Override
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+	}
+
+	@Override
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+	}
+}
