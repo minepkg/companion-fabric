352485524090f06f7ea4d9f992cf48535c837e98
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2025-03-13 16:33:08 +0100

    - port common to Java 8 - add 1_16, a backport of 1_17 - call common1_19_4's MinepkgCompanion directly from common using reflection, replacing the client/server entrypoints - update Glue to 0.0.7 - remove "mixins" from fabric.mod.json, load them manually in GluePreLaunch instead

--- common1_19_4/src/main/java/io/minepkg/companion/common1_19_4/MinepkgCompanion.java
+++ common1_19_4/src/main/java/io/minepkg/companion/common1_19_4/MinepkgCompanion.java
@@ -1,31 +1,13 @@
 package io.minepkg.companion.common1_19_4;
 
-import io.minepkg.companion.common.GlueMixinPlugin;
 import io.minepkg.companion.common1_19_4.mixin.ServerMetadataAccessor;
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.DedicatedServerModInitializer;
 
 import java.util.Optional;
 
-public class MinepkgCompanion implements ClientModInitializer, DedicatedServerModInitializer {
-	public void init() {
-		if (GlueMixinPlugin.testMinecraft(">=1.19.4")) {
-			io.minepkg.companion.common.MinepkgCompanion.INSTANCE.versionSetter = (metadata, version) -> {
-				io.minepkg.companion.common.MinepkgCompanion.LOGGER.info("minepkg companion for Minecraft >= 1.19.4 ready");
-				((ServerMetadataAccessor) (Object) metadata).setVersion(Optional.of(version));
-			};
-		}
-	}
-
-	// Client/DedicatedServerModInitializer run after all ModInitializer, guaranteeing that MinepkgCompanion.INSTANCE is already set
-
-	@Override
-	public void onInitializeClient() {
-		init();
-	}
-
-	@Override
-	public void onInitializeServer() {
-		init();
+public class MinepkgCompanion {
+	public static void init() {
+		io.minepkg.companion.common.MinepkgCompanion.INSTANCE.versionSetter = (metadata, version) -> {
+			((ServerMetadataAccessor) (Object) metadata).setVersion(Optional.of(version));
+		};
 	}
 }
